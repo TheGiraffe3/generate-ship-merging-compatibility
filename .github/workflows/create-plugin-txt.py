2f4e4dfb22e1034a10d88b44044e95ccbf1fbabd
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2025-12-04 11:38:04 +0300

    create a plugin.txt and icon

---
+++ .github/workflows/create-plugin-txt.py
@@ -0,0 +1,17 @@
+import os
+
+def run():
+	plugin = os.environ['PLUGIN']
+    plugin = plugin[0].upper() + plugin[1:]
+    plugintxt = '' + \
+		'name "' + plugin + ' Ship Merging Compatibility"\n' +\
+		'about "This plugin provides compatibility between ' + plugin + ' and ship.merging."\n' +\
+		'about "Thanks to zuckung for creating the icon and the script to generate this."\n' +\
+		'authors\n' +\
+		'	Loymdayddaud\n' +\
+		'	zuckung\n'
+	with open("plugin.txt", 'a') as fh:
+		fh.write(plugintxt)
+
+if __name__ == '__main__':
+	run()
--- .github/workflows/generate.py
+++ .github/workflows/generate.py
@@ -211,13 +211,14 @@ def write_ships(tiers, obj, ship_exclude, nodes_exclude):
 
 def write_mission(ship_names, data_folder):
 	
+	data_folder_capitalized = data_folder[0].upper() + data_folder[1:]
 	# mission start text
 	missiontxt = '' + \
 		'mission "upgrade ships ' + data_folder + '"\n' +\
 		'	job\n' +\
 		'	repeat\n' +\
-		'	name "(' + data_folder + ' compat: Upgrade Ships)"\n' +\
-		'	"description" "Combine a ' + data_folder + ' ship +3 base versions of that ship into one of a higher tier, up to a max of T10."\n' +\
+		'	name "(' + data_folder_capitalized + ': Upgrade Ships)"\n' +\
+		'	"description" "Combine a ' + data_folder_capitalized + ' ship + 3 base versions of that ship into one of a higher tier, up to a max of T10."\n' +\
 		'	color selected "ship_upgrades job: selected"\n' +\
 		'	color unselected "ship_upgrades job: unselected"\n' +\
 		'	source "su_planet1"\n' +\
