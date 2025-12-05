70f96de792f3699743239c41a9e8618c8368ad15
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2025-12-05 10:48:21 +0300

    also use a newline

--- .github/workflows/generate.py
+++ .github/workflows/generate.py
@@ -136,7 +136,7 @@ def write_ships(tiers, obj, ship_exclude, nodes_exclude):
 						new_attributes += line[:pos2+1] + ' ' + str(float(number) * (tier + 1)) + '\n'
 					except:
 						print('irregular line: ' + line)
-						new_attributes += line
+						new_attributes += line + '\n'
 				else:
 					spaced = line.split(' ')
 					number = spaced[1]
@@ -146,7 +146,7 @@ def write_ships(tiers, obj, ship_exclude, nodes_exclude):
 						new_attributes += spaced[0] + ' ' + str(float(number) * (tier + 1)) + '\n'
 					except:
 						print('irregular line: ' + line)
-						new_attributes += line
+						new_attributes += line + '\n'
 			# rename with tier
 			pos1 = part1.find('\n')
 			oldnameline = part1[:pos1-1]
