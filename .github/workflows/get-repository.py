9ff9fcb842a1951d78ff3f1e60d2c82254c62de7
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2025-12-09 07:26:11 +0300

    trim the extra information

--- .github/workflows/get-repository.py
+++ .github/workflows/get-repository.py
@@ -6,11 +6,12 @@ def run():
 	if reposplitted[0] == '### Repository':
 		print('		SUCCESS: Newly created issue is a compatibility request.')
 	print('REPOSITORY TO DOWNLOAD:')
-	repo = reposplitted[2]
+	repofull = reposplitted[2]
+	reponamesplit = repofull.split('/')
+	repo = reponamesplit[1] + '/' + reponamesplit[2]
 	print(repo)
 	with open("repository", 'a') as fh:
 		print(f'{repo}', file=fh)
-	reponamesplit = repo.split('/')
 	reponame = reponamesplit[1]
 	print(reponame)
 	with open("repository_name", 'a') as fh:
