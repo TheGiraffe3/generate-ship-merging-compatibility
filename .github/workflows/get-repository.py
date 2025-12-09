cd84e3f1beb7367718673b1627e4116e5c3b5fcf
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2025-12-09 08:05:38 +0300

    more detailed messages

--- .github/workflows/get-repository.py
+++ .github/workflows/get-repository.py
@@ -10,13 +10,17 @@ def run():
 	reponamesplit = repofull.split('/')
 	repo = reponamesplit[0] + '/' + reponamesplit[1]
 	repo = repo.rstrip('\n')
+	print(repo)
+	print()
 	with open("repository", 'a') as fh:
 		print(f'{repo}', file=fh)
 	print(f"Created repository file with value of {repo}")
+	print()
 	reponame = reponamesplit[1]
 	with open("repository_name", 'a') as fh:
 		fh.write(reponame)
 	print(f"Created repository_name file with value of {reponame}")
+	print()
 	datapath = ''
 	for i in range(1, len(reponamesplit)):
 		datapath += reponamesplit[i] + '/'
@@ -24,6 +28,7 @@ def run():
 	with open("datapath", 'a') as fh:
 		fh.write(datapath)
 	print(f"Created datapath file with value of {datapath}")
+	print()
 	if len(reponamesplit) > 3:
 		multipluginname = reponamesplit[3]
 		print(f"Created multipluginnamefile with value of {multipluginname}")
